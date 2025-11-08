544d18567768560e0acc06316e2fb5997e28b54b
Will <willscottuk@users.noreply.github.com>
2025-11-08 11:25:01 +0000

    Simplify browser startup in entrypoint.sh
    Removed browser detection logic and directly invoked google-chrome.
    
    Signed-off-by: Will <willscottuk@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,22 +11,9 @@ x11vnc -display $DISPLAY -rfbport $VNC_PORT -forever -passwd $VNC_PASSWORD &
 while ! pgrep "x11vnc" > /dev/null; do
     sleep .1
 done
-
-# Detect which browser to use
-if command -v google-chrome &> /dev/null; then
-    CHROME_CMD="google-chrome"
-elif command -v chromium-browser &> /dev/null; then
-    CHROME_CMD="chromium-browser"
-else
-    echo "Error: No browser found (neither google-chrome nor chromium-browser)"
-    exit 1
-fi
-
-echo "Starting browser: $CHROME_CMD"
-
-$CHROME_CMD --no-sandbox \
-	--disable-dev-shm-usage \
-	--disable-gpu \
-	--remote-debugging-port=$DEBUG_PORT \
-	--remote-debugging-address=0.0.0.0 \
-	--no-default-browser-check
+google-chrome --no-sandbox \
+    --disable-dev-shm-usage \
+    --disable-gpu \
+    --remote-debugging-port=$DEBUG_PORT \
+    --remote-debugging-address=$0.0.0.0 \
+    --no-default-browser-check
